1eb9bedea99bcb41f0801b0b90758b306025fac3
Mikhail Ponomarenko <mishashmidt0@gmail.com>
2023-11-13 21:50:07 +0300

    fix

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,7 @@
-import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
-import { NextResponse } from "next/server";
+import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   publicRoutes: ["/api/uploadthing"],
-  afterAuth(auth, req, evt) {
-    // handle users who aren't authenticated
-    if (!auth.userId && !auth.isPublicRoute) {
-      return redirectToSignIn({ returnBackUrl: req.url });
-    }
-    // redirect them to organization selection page
-    if (
-      auth.userId &&
-      !auth.orgId &&
-      req.nextUrl.pathname !== "/org-selection"
-    ) {
-      const orgSelection = new URL("/org-selection", req.url);
-
-      return NextResponse.redirect(orgSelection);
-    }
-  },
 });
 
 export const config = {
