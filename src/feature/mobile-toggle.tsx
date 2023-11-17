2b2aadc290d21996ded80d8f7d82f9ad795acec0
Mikhail Ponomarenko <mishashmidt0@gmail.com>
2023-11-18 00:20:18 +0300

    add prisma message

---
+++ src/feature/mobile-toggle.tsx
@@ -0,0 +1,31 @@
+import { Menu } from "lucide-react";
+
+import { Button } from "@/src/shared/ui/button";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetTrigger,
+} from "@/src/shared/ui/sheet";
+import { NavigationSidebar } from "@/src/widgets/navigation/navigation-sidebar";
+import { ServerSidebar } from "@/src/widgets/server/server-sidebar";
+interface MobileToggleProps {
+  serverId: string;
+}
+export const MobileToggle = ({ serverId }: MobileToggleProps) => {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant={"ghost"} size={"icon"} className={"md:hidden"}>
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side={"left"} className={"p-0 flex gap-0"}>
+        <div className={"w-[72px]"}>
+          <NavigationSidebar />
+        </div>
+        <ServerSidebar serverId={serverId} />
+      </SheetContent>
+    </Sheet>
+  );
+};
